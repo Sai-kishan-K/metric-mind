c17c9f01d38b120a2831d154b2d53c75025fcd95
Sai Kishan <saikishan@Sais-MacBook-Air.local>
2026-05-17 23:47:34 +0200

    Add frontend project files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// above codes are default tailwind config
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
